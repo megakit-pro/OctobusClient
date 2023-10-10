92ceedb3cc6a56eaaac43836da80083ac3bb9140
Anton Stremovskyy <stremovskyy@me.com>
2023-10-10 11:55:50 +0300

    init package

--- Package.swift
+++ Package.swift
@@ -11,11 +11,16 @@ let package = Package(
             name: "OctobusClient",
             targets: ["OctobusClient"]),
     ],
+    dependencies: [
+        .package(name: "Gzip", url: "https://github.com/1024jp/GzipSwift", from: "6.0.1"),
+        .package(name: "Starscream", url: "https://github.com/daltoniam/Starscream", from: "4.0.4")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "OctobusClient"),
+            name: "OctobusClient",
+            dependencies: ["Gzip", "Starscream"]),
         .testTarget(
             name: "OctobusClientTests",
             dependencies: ["OctobusClient"]),
