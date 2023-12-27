87b73a0fd06ec07c78fdbf5ccddc4a4f8e843991
Anton Stremovskyy <stremovskyy@me.com>
2023-12-27 12:42:20 +0200

    fixed decoding error

--- Sources/OctobusClient/Models/Server/ServerUser.swift
+++ Sources/OctobusClient/Models/Server/ServerUser.swift
@@ -10,13 +10,16 @@ import Foundation
 /// `ServerUser` is a structure that represents a user on the server.
 /// It conforms to the `Codable` protocol for easy encoding and decoding.
 public struct ServerUser: Codable {
-    public let id: Int
+    public let id: Int?
     /// An `Int` representing the ID of the user.
     public let userID: Int
     /// A `UserType` representing the type of the user.
     public let userType: UserType
     /// An optional `String` representing the name of the user.
     public let name: String?
+    
+    ///
+    public let uuid: String?
 
     /// An enumeration that represents the coding keys for the `ServerUser` structure.
     enum CodingKeys: String, CodingKey {
@@ -24,6 +27,7 @@ public struct ServerUser: Codable {
         case userID = "user_id"
         case userType = "user_type"
         case name
+        case uuid
     }
 }
 
